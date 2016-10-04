429b79aa3e99678e073c9c960fdf1180201e3529
Home <asdalexey@yandex.ru>
2016-10-04 23:13:29 +0300

    create correct react hmr babel

--- server/index.js
+++ server/index.js
@@ -3,14 +3,15 @@ import path from 'path';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev.js';
 
 const app = express();
+const compiler = webpack(webpackConfig);
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+app.use(webpackMiddleware(compiler, { noInfo: true }));
+app.use(webpackHotMiddleware(compiler));
 
-app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
-});
+app.get('/*', (req, res) => (res.sendFile(path.join(__dirname, './index.html'))));
 
 app.listen('3000', () => console.log('Runnng on localhost:3000'));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,21 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
     devtool: 'eval-source-map',
-    entry: path.join(__dirname + '/client/index.js'),
+    entry: [
+        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+        path.join(__dirname, '/client/index.js'),
+    ],
     output: {
         path: '/',
+        publicPath: '/',
     },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
     module: {
         loaders: [
             {
